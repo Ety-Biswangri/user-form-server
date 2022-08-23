6ed0d3f5c24edb1d87ce1048591c8b2997e5e35f
Ety-Biswangri <etybgc02@gmail.com>
2022-08-23 19:04:02 +0600

    cors

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ require('dotenv').config();
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const corsOptions = {
+/* const corsOptions = {
     origin: '*',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 }
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); */
+
+app.use(cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://user-form-application.netlify.app/"); // Update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://user-form-application.netlify.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
 app.use(express.json());
 
 
