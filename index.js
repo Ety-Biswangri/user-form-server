b6b12725bc9ecbc8d3ff49056e96755d099889ab
Ety-Biswangri <etybgc02@gmail.com>
2022-08-23 19:07:56 +0600

    c

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors(corsOptions)); */
 
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://user-form-application.netlify.app/");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     next();
 });
 
