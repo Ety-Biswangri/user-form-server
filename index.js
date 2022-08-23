a5c01c713364000c905b2087f5859d691f2af9ad
Ety-Biswangri <etybgc02@gmail.com>
2022-08-23 19:09:16 +0600

    cors

--- index.js
+++ index.js
@@ -13,13 +13,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors(corsOptions)); */
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
-    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-    next();
-});
-
+app.use(cors());
 app.use(express.json());
 
 
