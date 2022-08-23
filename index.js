ae7a104e137a837d0da439d2da0574138f82de16
Ety-Biswangri <etybgc02@gmail.com>
2022-08-23 15:31:48 +0600

    cors

--- index.js
+++ index.js
@@ -5,19 +5,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
-const corsConfig = {
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
-app.use(express.json());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization");
-    next();
-});
+app.use(cors());
 app.use(express.json());
 
 
